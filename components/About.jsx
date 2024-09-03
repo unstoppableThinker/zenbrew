160be524313087be0b14b3a82c0fd3676131a9bb
unstoppableThinker <yves.13507@gmail.com>
2024-09-02 21:13:17 -0400

    zenbrew-3 menu section

--- components/About.jsx
+++ components/About.jsx
@@ -53,49 +53,49 @@ const About = () => {
 	}, []);
 
   return <section className="overflow-hidden bg-primary">
-			<div ref={scrollTriggerRef}>
-				<div ref={scrollableSectionRef} className="h-screen w-[300vw] flex relative">
-					{data.map((item, index) => {
-						return (
-							<div key={index} className="w-screen h-screen flex flex-col justify-center items-center relative">
-								<div className="container mx-auto">
-									<div className="flex gap-[30px] relative">
-										<div className="flex-1 flex flex-col justify-center items-center">
-											<Badge containerStyles='w-[180px] h-[180px]' />
-											<div className="max-w-[560px] text-center">
-												<h2 className="h2 text-white mb-4">
-													<span className="mr-4">
-														{item.title.split(' ')[0]}
-													</span>
-													<span className="text-accent">
-														{item.title.split(' ')[1]}
-													</span>
-												</h2>
-												<div className="mb-8">
-													<Separator />
-												</div>
-												<p className="leading-relazed mb-16 px-8 xl:px-0">{item.description}</p>
-												<button className="btn">See more</button>
+		<div ref={scrollTriggerRef}>
+			<div ref={scrollableSectionRef} className="h-screen w-[300vw] flex relative">
+				{data.map((item, index) => {
+					return (
+						<div key={index} className="w-screen h-screen flex flex-col justify-center items-center relative">
+							<div className="container mx-auto">
+								<div className="flex gap-[30px] relative">
+									<div className="flex-1 flex flex-col justify-center items-center">
+										<Badge containerStyles='w-[180px] h-[180px]' />
+										<div className="max-w-[560px] text-center">
+											<h2 className="h2 text-white mb-4">
+												<span className="mr-4">
+													{item.title.split(' ')[0]}
+												</span>
+												<span className="text-accent">
+													{item.title.split(' ')[1]}
+												</span>
+											</h2>
+											<div className="mb-8">
+												<Separator />
 											</div>
+											<p className="leading-relazed mb-16 px-8 xl:px-0">{item.description}</p>
+											<button className="btn">See more</button>
 										</div>
-										<div className="hidden xl:flex flex-1 w-full h-[70vh] relative">
-											<Image
-												src={item.imgSrc}
-												fill
-												className="object-cover"
-												quality={100}
-												priority
-												alt=''
-											/>
-										</div>
+									</div>
+									<div className="hidden xl:flex flex-1 w-full h-[70vh] relative">
+										<Image
+											src={item.imgSrc}
+											fill
+											className="object-cover"
+											quality={100}
+											priority
+											alt=''
+										/>
 									</div>
 								</div>
 							</div>
-						);
-					})}
-				</div>
+						</div>
+					);
+				})}
 			</div>
-		</section>;
+		</div>
+	</section>;
 };
 
 export default About;
